7dbab674a8734ff3be3c1c60a2c95e8ff2566370
aizenbergivan <aizenbergivan@gmail.com>
2020-01-16 15:39:29 +0200

    Some changes

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h1>Congratulations! You are DevOps engineer!</h1>
+        <h1>Congratulations! Hi!</h1>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
     </section>
